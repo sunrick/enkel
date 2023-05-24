06a522bdecf5db570789b7854a0fd5850e76697b
Rickard Sunden <rickardsunden@gmail.com>
2023-05-24 11:33:09 -0400

    Add action class

--- lib/enkel.rb
+++ lib/enkel.rb
@@ -4,8 +4,6 @@ require "zeitwerk"
 loader = Zeitwerk::Loader.for_gem
 loader.setup
 
-require_relative "enkel/version"
-
 module Enkel
   class Error < StandardError; end
   # Your code goes here...
